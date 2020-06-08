022ff1717908939b309abfc8f457ce05341cdcad
Tal Shalif <tshalif+github@gmail.com>
2020-06-08 20:27:16 +0900

    misc: read TYPETIME, BREAKTIME from env vars

--- xwrits-deadman.sh
+++ xwrits-deadman.sh
@@ -13,7 +13,7 @@ if xwrits --help | grep activity-log >& /dev/null; then
     activity_log_arg="--activity-log $HOME/.xwrits_activity"
 fi
 
-xwrits $activity_log_arg  typetime=40 breaktime=4 +mouse  +breakclock +finger=german  +beep +clock after 5 multiply maxhands=6 after 5 flashtime=:0.4 after 1 maxhands=2000 &
+xwrits $activity_log_arg  typetime=${TYPETIME:-40} breaktime=${BREAKTIME:-4} +mouse  +breakclock +finger=german  +beep +clock after 5 multiply maxhands=6 after 5 flashtime=:0.4 after 1 maxhands=2000 &
 
 pid=$!
 
